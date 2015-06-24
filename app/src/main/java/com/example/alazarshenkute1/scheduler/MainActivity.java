20fc734ffb99afea4faff030c20a5a62baf6afac
alazar <a8nova@gmail.com>
2015-06-23 22:03:39 -0700

    working prototype

--- app/src/main/java/com/example/alazarshenkute1/scheduler/MainActivity.java
+++ app/src/main/java/com/example/alazarshenkute1/scheduler/MainActivity.java
@@ -1,66 +1,35 @@
+//
+// Alazar Shenkute
+// 6/19/2015
+// entry point of application
+/* An app that can be used by an Employer and an Employee to set
+    work schedule and availability, weekly
+  */
+//
+
 package com.example.alazarshenkute1.scheduler;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-
 
 public class MainActivity extends Activity {
-
-    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.activity_main );
-
-        // load image
-       /*******************************************************/
-        Button buttonLoadImage = (Button) findViewById( R.id.buttonLoadPicture);
-        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Intent i = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
-            }
-        });
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            ImageView imageView = (ImageView) findViewById(R.id.imgView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-
-        }
-
-
+    // invoke the log in activity if log in button pressed
+    public void logInButtonPressed( View view ){
+        startActivity( new Intent(this, LogIn.class ));
     }
+    // invoke sign up activity if sign up button pressed
+    public void signUpButtonPressed( View view ) {
+        startActivity( new Intent( this, SignUp.class ));
+    }
+
 
 }
