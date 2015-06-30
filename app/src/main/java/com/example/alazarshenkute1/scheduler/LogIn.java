730c9cc22a311e84bf17cb47ffed4c68bae0838b
alazar <a8nova@gmail.com>
2015-06-29 23:42:58 -0700

    added a Dashboard class - where you see weekly schedule's

--- app/src/main/java/com/example/alazarshenkute1/scheduler/LogIn.java
+++ app/src/main/java/com/example/alazarshenkute1/scheduler/LogIn.java
@@ -1,17 +1,18 @@
+//
+// Alazar Shenkute
+// 6/19/2015
+// These class is responsible for loging in the user
 package com.example.alazarshenkute1.scheduler;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created by alazarShenkute1 on 6/22/15.
- */
 public class LogIn extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // LogIn is the view instantiated by this activiy
-        setContentView( R.layout.login );
+        setContentView(R.layout.login );
 
     }
 }
--- app/src/main/java/com/example/alazarshenkute1/scheduler/SignUp.java
+++ app/src/main/java/com/example/alazarshenkute1/scheduler/SignUp.java
@@ -1,24 +1,63 @@
+//
+// Alazar Shenkute
+// 6/19/2015
+// These class is responsible for signing up the user
+// also saves the user email and password
+/*
+    key = email, since it has be unique
+    value = name, password, and picture of user
+ */
 package com.example.alazarshenkute1.scheduler;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends Activity {
 
+    // Keys for storing user data
+    private static final String EMAIL_KEY = "email";
+    private static final String NAME_KEY = "name";
+    private static final String PASSWORD_KEY = "passWord";
+    private static final String PICTURE_KEY = "picture";
+
+    // name of XML file in which all the data will be saved
+    private SharedPreferences preferences = null;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
-       // sgin up is the view instantiated by this activity
-        setContentView( R.layout.signup );
+       // sginup is the view instantiated by this activity
+        setContentView(R.layout.signup);
     }
 
-    // MUST pass a View argument or it doesnt work
-    // I dont know why
-    public void goButtonPressed( View view ){
-        // blowing up NEED TO BE FIXED
-        startActivity( new Intent(this, UploadPicture.class ) );
+    // MUST pass a View argument or it doesn't work
+    // I don't know why
+    public void submitButtonPressed( View view ){
+        // validate data
+        // save data
+         EditText[] editText =
+                {
+                        (EditText)findViewById( R.id.email ),
+                        (EditText)findViewById( R.id.name ),
+                        (EditText)findViewById( R.id.passwd )
+                };
+        preferences = getPreferences( MODE_PRIVATE );
+        SharedPreferences.Editor editor = preferences.edit(); // allows me to save key/value pairs
+        editor.putString( EMAIL_KEY, editText[0].getText().toString() );
+        editor.putString( NAME_KEY, editText[1].getText().toString() );
+        editor.putString(PASSWORD_KEY, editText[2].getText().toString());
+        editor.commit();
+
+        //---display file saved message---
+        Toast.makeText(getBaseContext(),
+                "saved!",
+                Toast.LENGTH_SHORT).show();
+
+        //startActivity( new Intent( this, UploadPictu2re.class ) );
     }
 
 }
